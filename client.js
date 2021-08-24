bb8540ca4f089c44cec642df78cb35cebb0e917d
AndrewEhrhardt <andyehrhardt@gmail.com>
2021-08-24 15:14:15 -0500

    accessEmplooyeeData function completed bonusCalculator started

--- client.js
+++ client.js
@@ -41,3 +41,56 @@ const employees = [
 // Ask questions when you don't.
 
 console.log( employees );
+
+// Hi Andy
+/*
+Those who have a rating of a 2 or below should not receive a bonus.
+Those who have a rating of a 3 should receive a base bonus of 4% of their base annual income.
+Those who have a rating of a 4 should receive a base bonus of 6% of their base annual income.
+Those who have a rating of a 5 should receive a base bonus of 10% of their base annual income.
+If their employee number is 4 digits long, this means they have been with the company for longer than 15 years, and should receive an additional 5%.
+However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%.
+No bonus can be above 13% or below 0% total.
+*/
+
+// 
+// function that takes in the employee object as an argument, returns modified object. 
+// New object should have 
+// name 
+// bonusPercentage, 
+// totalCompensation 
+// total bonus
+// Loop through the array passed into the function to access each employee
+// Then call a new function to access employeeNumber, annual salary and review rating of each object passed in
+// 
+//
+//
+
+function accessEmployeeData(employeeList){
+  console.log('in accessEmployeeData');
+  let employeeBonusArray = []; // empy array to store new objects
+  for (let employee of employeeList){
+    console.table(employee);
+    // call function to create new object
+    // push new object into employeeBonusArray
+    employeeBonusArray.push(bonusCalculator(employee));
+  }
+}
+
+function bonusCalculator(employeeObj){
+  console.log('in bonus');
+  let bonus = 0;
+  let bonusPercentage = 0;
+  let employeeBonusStatement = {
+    name: employeeObj.name,
+    totalCompensation: parseInt(employeeObj.annualSalary) + bonus,
+    // do we need an if statement to contain total bonuses?
+    bonusPercentage: bonusPercentage, // add calculation here
+    // will a switch statement work here? 
+    totalBonus: bonus, // add calculation here
+  };
+  if ()
+}
+
+console.log(accessEmployeeData(employees));
+
